4babb6c03ce86aa76242a53fc1e90cbdba367c7f
AndreaDeGiorgi00 <andrea.adedegiorgi@gmail.com>
2023-02-10 15:10:02 +0100

    al click del bottone vine stampato in un altra pagina la password generata

--- functions/functions.php
+++ functions/functions.php
@@ -1,10 +1,15 @@
 <?php 
+
 function getPassword($n){
-    $allcharacters = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+    $allcharacters = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!£$%&/()=?^[]*@#";
     $password = "";
     for($i = 0 ; $i < $n ; $i++){
-        $password .= $allcharacters[random_int(0 , strlen('allcharacters'))];
+        $password .= $allcharacters[random_int(0 , strlen($allcharacters))];
     };
+
+    session_start();
+    $_SESSION['password'] = $password;
+
     return $password;
 }
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php 
-include 'functions/functions.php'
+    include 'functions/functions.php';
+    
+    
+    if(isset(  $_GET['numero_caratteri'])){
+        getPassword($_GET['numero_caratteri']);
+        header('location: yourPassword.php');
+    } ;
+    
+    
 
+    
 ?>
 
 <!DOCTYPE html>
@@ -9,7 +18,7 @@ include 'functions/functions.php'
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>generatore password</title>
 </head>
 <body>
@@ -21,10 +30,7 @@ include 'functions/functions.php'
             <button>invia</button>
         </form>
 
-        <?php if ($_GET) : ?>
-            <h3 class="mt-5">la tua password sarà </h3>
-            <h1><?= getPassword($_GET['numero_caratteri']) ?></h1>
-        <?php endif; ?>
+        
     </div>
 </body>
 </html>
